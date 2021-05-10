75131df52bac97e4e56db4da078a897e2060299a
Amopho <amopho78@gmail.com>
2021-05-10 11:32:56 +0200

    js script

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+window.onload = function () {
+  let secs = 00;
+  let tens = 00;
+  let grabSecs = document.getElementById("seconds");
+  let grabTens = document.getElementById("tens");
+  let grabStart = document.getElementById("button-start");
+  let grabStop = document.getElementById("button-stop");
+  let grabReset = document.getElementById("button-reset");
+};
 
+grabStart.onclick = function () {
+  clearInterval();
+};
