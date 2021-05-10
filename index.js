d7efd9ae0f96d3e223d9666a1cef761619e32a70
Amopho <amopho78@gmail.com>
2021-05-10 11:51:59 +0200

    event listeners

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ window.onload = function () {
   let grabStart = document.getElementById("button-start");
   let grabStop = document.getElementById("button-stop");
   let grabReset = document.getElementById("button-reset");
+  let interval;
 };
 
 grabStart.onclick = function () {
-  clearInterval();
+  clearInterval(interval);
+  interval = setInterval(startTimer, 10);
+  // setInterval() function will execute the timer once every 1 second
+  //   clearInterval() will stop the timer
+};
+
+grabStop.onclick = function () {
+  clearInterval(interval);
 };
